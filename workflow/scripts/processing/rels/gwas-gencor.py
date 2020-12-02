8c6ba7773e941ab40907830328a0cc6f127dab36
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2020-12-02 15:01:52 +0000

    using different genetic correlation data

--- workflow/scripts/processing/rels/gwas-gencor.py
+++ workflow/scripts/processing/rels/gwas-gencor.py
@@ -29,8 +29,54 @@ meta_id = args.name
 
 FILE = get_source(meta_id,1)
 
-
 def get_gwas_data():
+    # create the data
+    gwas_api_url = "http://gwasapi.mrcieu.ac.uk/gwasinfo"
+    logger.info("Getting gwas data from {}", gwas_api_url)
+    gwas_res = requests.get(gwas_api_url).json()
+    df = pd.DataFrame(gwas_res)
+    df = df.T.fillna("")
+    logger.info(df.shape)
+    #only want uk biobank traits
+    df = df[df['id'].str.startswith('ukb-')]
+    logger.info(df.shape)
+    logger.info("\n {}",df.head())
+    return df
+
+def process(gwas_df):
+    logger.info("Reading {}", FILE)
+    gc_df = pd.read_csv(os.path.join(dataDir, FILE), delimiter=",",nrows=10)
+    logger.info(gc_df.head())
+    logger.info(gc_df.shape)
+    regex = r".*?<a.*?>(.*?)</a>.*"
+    n1=[]
+    n2=[]
+    for values in gc_df['Phenotype 1']:
+        name = re.search(regex,values)
+        n1.append(name.group(1))
+    for values in gc_df['Phenotype 2']:
+        name = re.search(regex,values)
+        n2.append(name.group(1))
+    gc_df['phenotype_name_1']=n1
+    gc_df['phenotype_name_2']=n2
+    logger.info("\n{}",gc_df)
+    logger.info(gc_df.shape)
+    
+    #merge with opengwas df
+    merge_df = pd.merge(gc_df,gwas_df[['id','trait']],left_on="phenotype_name_1",right_on="trait",how='left')
+    merge_df.rename(columns={'id':'phenotype_1_id'},inplace=True)
+    merge_df.drop('trait',inplace=True, axis=1)
+
+    merge_df = pd.merge(gc_df,gwas_df[['id','trait']],left_on="phenotype_name_2",right_on="trait",how='left')
+    merge_df.rename(columns={'id':'phenotype_2_id'},inplace=True)
+    merge_df.drop(['trait'],inplace=True, axis=1)
+
+    logger.info("\n{}",merge_df)
+    logger.info(merge_df.shape)
+
+
+
+def get_gwas_data2():
     print("Getting GWAS IDs...")
     # create the data
     gwas_api_url = "http://gwasapi.mrcieu.ac.uk/gwasinfo"
@@ -65,7 +111,7 @@ def get_gwas_data():
     return idDic
 
 
-def process(idDic):
+def process2(idDic):
     logger.info("Reading {}", FILE)
     df = pd.read_csv(os.path.join(dataDir, FILE), delimiter="\s+")
     logger.info(df.head())
@@ -132,7 +178,14 @@ def process(idDic):
     df = df.rename(columns={"p1_gwas": "source", "p2_gwas": "target"})
     create_import(df=df, meta_id=meta_id)
 
+def testing():
+    df = pd.read_csv(os.path.join(dataDir, FILE), delimiter="\s+",nrows=1000)
+    print(df.head())
+    print(df['rg'].max())
+
 
 if __name__ == "__main__":
-    idDic = get_gwas_data()
-    process(idDic)
+    df = get_gwas_data()
+    #df=''
+    process(df)
+    #testing()
