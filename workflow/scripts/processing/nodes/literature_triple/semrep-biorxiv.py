4a4573307c696475dc062c826238bb5b09195812
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2021-02-03 09:41:59 +0000

    added sub-pred-obj name back to triples

--- workflow/scripts/processing/nodes/literature_triple/semrep-biorxiv.py
+++ workflow/scripts/processing/nodes/literature_triple/semrep-biorxiv.py
@@ -30,16 +30,28 @@ def make_id(row,sub_type):
         id_val = row[sub_type+'_gene_id']
     return id_val
 
+def make_name(row,sub_type):
+    id_val = row[sub_type+'_name']
+    if pd.isna(row[sub_type+'_name']):
+        id_val = row[sub_type+'_gene_name']
+    return id_val
+
 def process():
     logger.info("loading semrep data... {}",SEM)
     sem_df = pd.read_csv(os.path.join(dataDir, SEM), sep=",", compression="gzip")
 
-    #create new ids 
+    #create new ids, if a gene standard id is empty
     logger.info('Dealing with IDs')
     sub_id = sem_df.apply(lambda row : make_id(row,'sub'), axis = 1)
     obj_id = sem_df.apply(lambda row : make_id(row,'obj'), axis = 1)
     sem_df['sub_id_all']=sub_id
     sem_df['obj_id_all']=obj_id
+
+    #create new names, if a gene standard name is empty
+    sub_name = sem_df.apply(lambda row : make_name(row,'sub'), axis = 1)
+    obj_name = sem_df.apply(lambda row : make_name(row,'obj'), axis = 1)
+    sem_df['sub_name_all']=sub_name
+    sem_df['obj_name_all']=obj_name
 
     # need to split subject and object ids by ,
     logger.info(sem_df.shape)
@@ -57,7 +69,7 @@ def process():
     logger.info(sem_df.shape)
 
     sem_id = sem_df['sub_id_all']+':'+sem_df['pred']+':'+sem_df['obj_id_all']
-    sem_name = sem_df['sub_id_all']+' '+sem_df['pred']+' '+sem_df['obj_id_all']
+    sem_name = sem_df['sub_name_all']+' '+sem_df['pred']+' '+sem_df['obj_name_all']
     logger.debug(sem_id)
     sem_df['id']=sem_id
     sem_df['name']=sem_name
@@ -74,7 +86,7 @@ def process():
     ]
     sem_df = sem_df[keep_cols]
     sem_df.rename(columns={'pred':'predicate','sub_id_all':'subject_id','obj_id_all':'object_id'},inplace=True)
-    sem_df.drop_duplicates(inplace=True)
+    sem_df.drop_duplicates(subset=['id'],inplace=True)
     logger.info("\n{}", sem_df)
     logger.info(sem_df.shape)
 
