3e51de9261936b2fcb7def68b7ff26e190ec4e17
Ben Elsworth <be15516@bristol.ac.uk>
2021-05-05 09:56:33 +0100

    modified semmed source script to filter no novelty

--- workflow/scripts/source/get_semmeddb.py
+++ workflow/scripts/source/get_semmeddb.py
@@ -3,6 +3,9 @@ import sys
 import pandas as pd
 import wget
 from loguru import logger
+from workflow.scripts.utils.general import copy_source_data
+
+data_name = 'semmeddb'
 
 #load semmemd tab separated predication to filter and create smaller version
 PREDICATION_FILE = sys.argv[1]
@@ -11,39 +14,8 @@ PREDICATION_FILE = sys.argv[1]
 # due to the license requirements, there doesn't seem to be a way to download the data easily
 # need to download the PREDICATION and CITATION CSV files
 
-
 def process():
-    #applying same filters as MELODI Presto
-    predIgnore = [
-        "PART_OF",
-        "ISA",
-        "LOCATION_OF",
-        "PROCESS_OF",
-        "ADMINISTERED_TO",
-        "METHOD_OF",
-        "USES",
-        "compared_with",
-    ]
-    # typeFilterList = ["aapp","dsyn","enzy","gngm","chem","clnd","horm","hops","inch","orch"]
-    typeFilterList = [
-        "aapp",
-        "clnd",
-        "clna",
-        "dsyn",
-        "enzy",
-        "gngm",
-        "chem",
-        "clnd",
-        "horm",
-        "hops",
-        "inch",
-        "orch",
-        "phsu",
-    ]
-
-    #load predicate data
-    logger.info('loading data...')
-    df = pd.read_csv(PREDICATION_FILE,sep='\t')
+    df = pd.read_csv(PREDICATION_FILE)
     col_names=[
         'predication_ID',
         'sentence_ID',
@@ -60,25 +32,23 @@ def process():
         'x',
         'y',
         'z',
-        ]
-    
+    ]
     df.columns=col_names
-    logger.info(df.shape)
-    #filter on predicates
-    df = df[~df.predicate.isin(predIgnore)]
-    logger.info(df.shape)
-    #filter on types
-    df = df[df.subject_type.isin(typeFilterList)]
-    df = df[df.object_type.isin(typeFilterList)]
-    logger.info(df.shape)
-
-    df.to_csv(PREDICATION_FILE+'.filter',compression='gzip',index=False)
-
-def filter():
-    df = pd.read_csv(PREDICATION_FILE,nrows=1000)
     logger.info(f'\n{df.head()}')
+    logger.info(f"\n{df['subject_novelty'].value_counts()}")
+    logger.info(f"\n{df['object_novelty'].value_counts()}")
+    sub_nov = df[df.subject_novelty==0]['subject_name'].value_counts()
+    logger.info(sub_nov)
+    logger.info(df.shape)
+
+    # filter to only keep rows with both subject and object novelty of 1
+    df = df[(df['subject_novelty']==1) & (df['object_novelty']==1)]
+    logger.info(df.shape)
+    logger.info(f'\n{df.head()}')
+    filename = f'/tmp/{PREDICATION_FILE}.filter.gz'
+    df.to_csv(filename,index=False)
+    copy_source_data(data_name=data_name,filename=filename)
 
 if __name__ == "__main__":
-    #process()
-    filter()
+    process()
     
