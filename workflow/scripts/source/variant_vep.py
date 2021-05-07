9c77d84ad72df8f4026f3ef3e9dc42add509114b
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2021-05-07 13:49:16 +0100

    vep work

--- workflow/scripts/source/variant_vep.py
+++ workflow/scripts/source/variant_vep.py
@@ -5,12 +5,13 @@ import datetime
 import requests
 
 from workflow.scripts.utils import settings
-from workflow.scripts.utils.general import get_data_from_server
+from workflow.scripts.utils.general import get_data_from_server, copy_source_data
 from loguru import logger
 
 env_configs = settings.env_configs
 
 data_dir = "/tmp/epigraph-build/vep"
+data_name = 'vep'
 os.makedirs(data_dir,exist_ok=True)
 
 today = datetime.date.today()
@@ -23,10 +24,14 @@ variant_data = [
     },
     {
         'name':'mr-eve',
-        'file':'opengwas/opengwas-tophits-2020-10-13.csv',
-        'snp_col':'rsid'
+        'file':'mr-eve/21_03_10/variants.csv.gz',
+        'snp_col':'variantId:ID(variant)'
     },
-    #{}
+    {
+        'name':'xqtl',
+        'file': 'xqtl-processed/xqtl_single_snp.csv',
+        'snp_col':'rsid'
+    }
 ]
 
 # setup
@@ -34,6 +39,8 @@ variant_data = [
 # docker run -t -i -v /data/vep_data:/opt/vep/.vep ensemblorg/ensembl-vep perl INSTALL.pl -a cf -s homo_sapiens -y GRCh37
 
 vep_data_dir='/data/vep_data'
+
+v_file=f'{vep_data_dir}/variants-{today}.txt'
 
 def get_existing():
     url = 'https://api.epigraphdb.org/cypher'
@@ -50,17 +57,22 @@ def get_data():
     for i in variant_data:
         get_data_from_server(dataName=i['file'],outDir=data_dir)
 
-def process_variants(variant_data):
+def process_variants():
+    variant_df=pd.DataFrame(columns = ['rsid'])
     for i in variant_data:
         logger.info(i)
         logger.info(f"processing {i['name']}")
-        df = pd.read_csv(i['file'],low_memory=False)
+        f = os.path.basename(i['file'])
+        df = pd.read_csv(f"{data_dir}/{f}",low_memory=False)
         df = df[i['snp_col']]
         df.drop_duplicates(inplace=True)
         logger.info(df.head())
-    #df.head.to_csv(f'{vep_data_dir}/variants-{today}.txt',index=False,header=False)
+        variant_df = pd.concat([variant_df,df])
+        logger.info(variant_df.shape)
+    variant_df.drop_duplicates(inplace=True)
+    variant_df.to_csv(v_file,index=False,header=False,sep=' ')
 
-def run_vep(variant_dir,variant_file):
+def run_vep(variant_file):
     com="""
         docker run -t -i -v {vep_data_dir}:/opt/vep/.vep 
         ensemblorg/ensembl-vep ./vep --port 3337 --cache --fork 20 --assembly GRCh37 
@@ -72,19 +84,20 @@ def run_vep(variant_dir,variant_file):
     com = com.replace('\n',' ')
     logger.info(com)
     subprocess.call(com, shell=True)
+    
     #copy results 
-    com = f"cp /data/vep_data/vep-{today}.txt {env_configs['data_dir']}/vep/"
-    subprocess.call(com, shell=True)
+    copy_source_data(data_name=data_name, filename=f"/data/vep_data/vep-{today}.txt")
+
+    #com = f"cp /data/vep_data/vep-{today}.txt {env_configs['data_dir']}/vep/"
+    #subprocess.call(com, shell=True)
 
 if __name__ == "__main__":
-    variant_data = [
-        {
-        'name':'opengwas',
-        'file':os.path.join(env_configs['data_dir'],'opengwas','opengwas-tophits-2020-10-13.csv'),
-        'snp_col':'rsid'
-        }
-    ]
-    #existing_df = get_existing()
-    #process_variants(variant_data)
-    #run_vep('/tmp',f'variants-{today}.txt')
-    get_data()
+    if os.path.exists(v_file):
+        logger.info(f'{v_file} exists')
+    else:
+        existing_df = get_existing()
+        get_data()
+        process_variants()
+    run_vep(os.path.basename(v_file))
+    #run_vep('variants-2021-05-06_head.txt')
+    
