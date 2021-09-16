e02f532e4e9ae5703473733d692ebabbc29e3d9f
Ben Elsworth <be15516@crashdown.epi.bris.ac.uk>
2021-09-16 11:51:17 +0100

    efo-lit-nlp

---
+++ workflow/scripts/processing/rels/efo-lit-nlp.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import pandas as pd
+import gzip
+import json
+from loguru import logger
+
+#################### leave me heare please :) ########################
+
+from workflow.scripts.utils.general import setup, get_source
+
+from workflow.scripts.utils.writers import (
+    create_constraints,
+    create_import,
+)
+
+# setup
+args, dataDir = setup()
+meta_id = args.name
+
+# args = the argparse arguments (name and data)
+# dataDir = the path to the working directory for this node/rel
+
+#######################################################################
+
+FILE = get_source(meta_id, 1)
+
+
+def process():
+    logger.info("Loading efo...{}", FILE)
+    df = pd.read_csv(
+        os.path.join(dataDir, FILE),
+        sep="\t",
+        names=["target", "source", "score"],
+    )
+    logger.info("\n{}", df.head())
+    logger.info(df.shape)
+    df.drop_duplicates(inplace=True)
+    logger.info(df.shape)
+    create_import(df=df, meta_id=meta_id)
+
+
+if __name__ == "__main__":
+    process()
